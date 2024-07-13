a617d01f35288cad3992ee2f72d8d88cc60faf5a
ezeifeanyiking <kingsleyfn41@gmail.com>
2024-07-13 23:06:05 +0100

    gcd

--- Day2/greatesCommonDivisorOfStrings.js
+++ Day2/greatesCommonDivisorOfStrings.js
@@ -1,4 +1,3 @@
-
 // For two strings s and t, we say "t divides s" if and only
 // if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 // Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
@@ -17,29 +16,25 @@
 // step 3 -> If str1 is greater than str2 -> check if str1 started with str2 (if so, remove the starting point is try again)
 // step 4 -> if str1 does not start with str2 return empty string
 
-function solution(str1, str2){
-    console.log(`param1 = ${str1}, param2 = ${str2}`)
+function solution(str1, str2) {
+  let str1Len = str1.length;
+  let str2Len = str2.length;
 
-    let str1Len = str1.length
-    let str2Len = str2.length
+  if (str2Len > str1Len) {
+    return solution(str2, str1);
+  }
 
-    if(str2Len > str1Len){
-        return solution(str2, str1)
-    }
+  if (str1 === str2) {
+    return str1;
+  }
+  let offset = str1.substring(0, str2.length);
+  let rem = str1.substring(str2.length);
 
-    if(str1 === str2){
-        console.log("in this block")
-        return str1;
-    }
-    let offset = str1.substring(0, str2.length);
-    let rem = str1.substring(str2.length)
+  if (offset === str2) {
+    return solution(rem, str2);
+  }
 
-    if(offset === str2){
-        return solution(rem,str2)
-    }
-
-    return ""
+  return "";
 }
 
-console.log(solution("ABCABC", "ABC"))
-
+console.log(solution("ABCABC", "ABC"));
